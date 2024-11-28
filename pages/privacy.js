2a30dccf947b448b0de43d58adbc78811a50f548
MaximumCrash <rejon@maximumcrash.com>
2024-11-27 20:31:13 -0800

    Add Privacy policy page

---
+++ pages/privacy.js
@@ -0,0 +1,60 @@
+export default function Privacy() {
+    return (
+        <div>
+<h1>Privacy Policy </h1>
+<h3>
+Effective Date: November 27, 2024</h3>
+
+<p>
+This Privacy Policy applies to your use of software products produced by Soft Not Weak, LLC, a Partnership LLC, as well as to the use of any and all Data collected by the Studio or provided by you in relation to your use of the same software products.
+
+</p>
+
+<h2>Contact</h2>
+<p>
+If you have questions about data protection, or if you have any requests for resolving issues with your personal data, we encourage you to primarily contact us at:
+email: contact@softnotweak.com
+</p>
+
+<h2>
+The Data you provide us directly
+</h2>
+<ul>
+    <li>Contact information (such as email address) via our Newsletter forms, Kickstarter, Discord, and other forms of outreach</li>
+        <li>Player name or tag</li>
+<li>Profile Information</li>
+<li>Data you provide in connection with surveys, promotions, sweepstakes, and other events</li>
+</ul>
+
+<h2>
+Data that is automatically collected
+</h2>
+<ul>
+    <li>
+    Play time and achievement information on accessible Game Platforms (such as Steam, Xbox, ect.)
+    </li>
+</ul>
+
+<h2>Our Promise</h2>
+<p>We do not sell or otherwise disclose any portion of our data to third-parties, with the exception of the vendor or Game Platform that provides their services as necessary to distribute our products.</p>
+
+<style>
+    {`
+    h1 {
+    font-size: 2.5rem;}
+        h2 {
+            font-size: 2rem;
+            }
+
+        h3 {
+        font-size: 1.5rem;
+        }
+        li {
+        list-style-type: disc outside none;
+        }
+    `}
+</style>
+
+</div>
+    )
+}
